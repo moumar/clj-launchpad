31101a7bc375fc741b73adfb927e29ccbd46fbf9
Guillaume Pierronnet <guillaume.pierronnet@gmail.com>
2012-01-22 14:09:32 +0100

    init

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject clj-launchpad "1.0.0-SNAPSHOT"
+  :description "Novation Launchpad library for clojure"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/clj_launchpad/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-launchpad.test.core
+  (:use [clj-launchpad.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
