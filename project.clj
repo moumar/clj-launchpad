7fe658d66ca971fd19c719a18bb285a8cdeb6ca9
dvberkel <daan.v.berkel.1980@gmail.com>
2015-06-08 16:29:03 +0200

    Set url to project on GitHub

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject clj-launchpad "0.2.0-SNAPSHOT"
   :description "Novation Launchpad library for clojure"
+  :url "https://github.com/moumar/clj-launchpad"
   :dependencies [[org.clojure/clojure "1.6.0"]])
