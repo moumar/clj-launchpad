87119ca1e981e385897872a68caaab4985bb995c
dvberkel <daan.v.berkel.1980@gmail.com>
2015-06-08 16:31:09 +0200

    Set licences to standard for clojure

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject clj-launchpad "0.2.0-SNAPSHOT"
   :description "Novation Launchpad library for clojure"
   :url "https://github.com/moumar/clj-launchpad"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
