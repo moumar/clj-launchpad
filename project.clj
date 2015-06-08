356308fc6fe46a12df592ce520689a43b38ac12a
dvberkel <daan.v.berkel.1980@gmail.com>
2015-06-08 16:27:56 +0200

    Set version to 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject clj-launchpad "0.1.0"
+(defproject clj-launchpad "0.2.0-SNAPSHOT"
   :description "Novation Launchpad library for clojure"
   :dependencies [[org.clojure/clojure "1.6.0"]])
